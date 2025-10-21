c6e95e4cb355622ee949a16eb85bab56b3628d7b
Abdallah Wael <bedowael365@gmail.com>
2025-10-21 15:14:27 +0300

    add workhole-rawafi domain to CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://work-hole.vercel.app', 'https://diethub-workhole.vercel.app', 'http://localhost:8081' ],
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://work-hole.vercel.app', 'https://diethub-workhole.vercel.app', 'http://localhost:8081', 'https://workhole-rawafi.vercel.app' ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With'],
